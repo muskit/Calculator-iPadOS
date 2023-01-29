5f6f7a8f372cd107f42005c2e824e23ffa3319d2
msk <15199219+muskit@users.noreply.github.com>
2023-01-29 12:03:54 -0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Alex on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
